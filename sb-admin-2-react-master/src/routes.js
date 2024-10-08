121e7c17c71e13702197902bdfc7377ba7f5c554
Samarjeet Singh <samkumar768@gmail.com>
2024-10-08 18:48:54 +0200

    Update routes.js

--- sb-admin-2-react-master/src/routes.js
+++ sb-admin-2-react-master/src/routes.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 // Pages
@@ -22,6 +22,20 @@ const LoadingSpinner = () => (
     </div>
 );
 
+// ProtectedRoute Component
+const ProtectedRoute = ({ element }) => {
+    const navigate = useNavigate();
+    const token = localStorage.getItem("token"); // Replace with your method of retrieving the token
+
+    useEffect(() => {
+        if (!token) {
+            navigate("/"); // Redirect to sign-in page if token is not present
+        }
+    }, [token, navigate]);
+
+    return element; // Render the element if the token exists
+};
+
 const AppRoutes = (props) => {
     const isLoading = useSelector((state) => state.loading.isLoading); // Access loading state from Redux
 
@@ -32,12 +46,12 @@ const AppRoutes = (props) => {
             ) : (
                 <Routes>
                     <Route path="/" element={ <SignIn /> } />
-                    <Route path="/dashboard" element={ <Dashboard /> } />
+                    <Route path="/dashboard" element={ <ProtectedRoute element={ <Dashboard /> } /> } />
                     <Route path="/signup" element={ <SignUp { ...props } /> } />
-                    <Route path="/charts" element={ <Charts /> } />
-                    <Route path="/cards" element={ <Cards /> } />
-                    <Route path="/quotes" element={ <InsuranceMainForm { ...props } /> } />
-                    <Route path="/viewQuote" element={ <InsuranceViewQuotes { ...props } /> } />
+                    <Route path="/charts" element={ <ProtectedRoute element={ <Charts /> } /> } />
+                    <Route path="/cards" element={ <ProtectedRoute element={ <Cards /> } /> } />
+                    <Route path="/quotes" element={ <ProtectedRoute element={ <InsuranceMainForm { ...props } /> } /> } />
+                    <Route path="/viewQuote" element={ <ProtectedRoute element={ <InsuranceViewQuotes { ...props } /> } /> } />
                     <Route path="*" element={ <NotFound /> } />
                 </Routes>
             ) }
